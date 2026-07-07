0e9834c99945b1101a6a28a2838a6f71fe484b92
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-07-07 13:48:52 +0100

    indenting fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function play() {
-  var buttons = document.querySelectorAll("#btn button");
-  for (let i=0;i<buttons.length;i++) {
-    buttons[i].style.display = "none";
-  }
+    var buttons = document.querySelectorAll("#btn button");
+    for (let i=0;i<buttons.length;i++) {
+        buttons[i].style.display = "none";
+    }
 }
