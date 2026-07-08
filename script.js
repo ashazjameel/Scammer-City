5edb5c8ca68e4b9024b05cc9939395883bdace57
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-07-08 17:39:42 +0100

    adding working flag

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function play() {
         buttons[i].style.display = "none";
     }
 }
+
+function hover(x) {
+    var f = x.querySelectorAll("div");
+    for (let i=0;i<f.length;i++) {
+        f[i].style.visibility = "visible";
+    }
+}
+
+function unhover(x) {
+    var f = x.querySelectorAll("div");
+    for (let i=0;i<f.length;i++) {
+        f[i].style.visibility = "hidden";
+    }
+}
