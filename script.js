21a104a5e5f2d8b7807a13f50eac5953ae04afca
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-07-09 11:20:36 +0100

    adding touchscreen support

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const touch = matchMedia("(hover: none)").matches;
+
 function play() {
     var buttons = document.querySelectorAll(".btn, .title");
     for (let i=0;i<buttons.length;i++) {
@@ -5,16 +7,20 @@ function play() {
     }
 }
 
-function hover(x) {
-    var f = x.querySelectorAll("div");
-    for (let i=0;i<f.length;i++) {
-        f[i].style.visibility = "visible";
+function hover(x, check) {
+	if (check || (!check && !touch)) {	
+        var f = x.querySelectorAll("div");
+        for (let i=0;i<f.length;i++) {
+            f[i].style.visibility = "visible";
+        }
     }
 }
 
-function unhover(x) {
-    var f = x.querySelectorAll("div");
-    for (let i=0;i<f.length;i++) {
-        f[i].style.visibility = "hidden";
+function unhover(x, check) {
+	if (check || (!check && !touch)) {	
+        var f = x.querySelectorAll("div");
+        for (let i=0;i<f.length;i++) {
+            f[i].style.visibility = "hidden";
+        }
     }
 }
