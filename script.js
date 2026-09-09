ea5e62d5090eb20894c2e76512097aeb921f3142
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-09-09 14:09:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+var vertexShaderSource;
+
 fetch("vertex-shader.glsl")
 	.then(response => response.text())
 	.then(text => console.log(text))
-	.then(text => {var vertexShaderSource = text});
+	.then(text => {vertexShaderSource = text});
 
 const touch = matchMedia("(hover: none)").matches;
 
