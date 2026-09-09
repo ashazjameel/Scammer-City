302147d3836f64dc307a0ec3aa4a5a9a9e4c75e7
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-09-09 14:05:06 +0100

    test code

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function unhover(x, check) {
 }
 
 function createShader(gl, type, source) {
+	console.log(source);
 	var shader = gl.createShader(type);
 	gl.shaderSource(shader, source);
 	gl.compileShader(shader);
