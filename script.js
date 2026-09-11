ecf2a3e09b09db6580e1b2be04998dfc9ab9bce8
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-09-11 12:25:00 +0100

    undo

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ function render() {
 	var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
 	var program = createProgram(gl, vertexShader, fragmentShader);
+	var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 	var positionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
@@ -94,7 +95,6 @@ function render() {
 	gl.useProgram(program);
 	gl.enableVertexAttribArray(positionAttributeLocation);
 
-	var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 	var size = 2;
 	var type = gl.FLOAT;
 	var normalize = false;
