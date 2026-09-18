9c34d814763bf83e7247637e0da6fb68d3762df6
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-09-18 12:06:09 +0100

    undo

--- script.js
+++ script.js
@@ -37,10 +37,6 @@ function unhover(x, check) {
     }
 }
 
-function fetchData() {
-	fetch("/api/test");
-}
-
 function createShader(gl, type, source) {
 	var shader = gl.createShader(type);
 	gl.shaderSource(shader, source);
